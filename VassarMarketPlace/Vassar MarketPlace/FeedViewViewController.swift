ae9bb7bd21e6193c6565ac900a695b7f4f121c5c
riikacharya-code <racharya@vassar.edu>
2022-04-30 22:42:40 -0400

    hi

--- VassarMarketPlace/Vassar MarketPlace/FeedViewViewController.swift
+++ VassarMarketPlace/Vassar MarketPlace/FeedViewViewController.swift
@@ -6,24 +6,73 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class FeedViewViewController: UIViewController {
+class FeedViewViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post = posts[indexPath.section]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        if indexPath.row == 0 {
+            let user = post["usernameLabel"] as! PFUser
+            cell.usernameLabel.text = user.username
 
+            cell.itemDescription.text = post["itemDescription"] as? String
+
+            let imageFile = post["itemImage"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+
+            cell.itemImage.af.setImage(withURL: url)
+
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AddCommentCell")!
+            
+            return cell
+        }
+    }
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    var selectedPost: PFObject!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        // user can dismiss keyboard by pulling down
+        tableView.keyboardDismissMode = .interactive
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKeys(["author", "comments", "comments.author"])
+        query.limit = 40
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
     }
-    */
 
 }
--- VassarMarketPlace/Vassar MarketPlace/LoginViewController.swift
+++ VassarMarketPlace/Vassar MarketPlace/LoginViewController.swift
@@ -18,8 +18,6 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-    
-    
     @IBAction func onSignIn(_ sender: Any) {
         let username = usernameTextField.text!
         let password = passwordTextField.text!
@@ -33,12 +31,9 @@ class LoginViewController: UIViewController {
         }
     }
     
-    
     @IBAction func onSignUp(_ sender: Any) {
         
     }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,16 +41,4 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
